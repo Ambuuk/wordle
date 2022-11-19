9dd00b1de520b224e9b48a680251d43954446bc2
Ambuuk <amarbold.u@netcapital.mn>
2022-11-19 15:36:09 +0800

    react shift

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Wordle from './Wordle';
+
+function App() {
+
+  function handleAddTodo(e){
+    const name = "qwer"
+
+    setTodos(prevTodos => {
+      
+    })
+  }
+
+  return (
+    <Wordle />
+  );
+}
+
+export default App;
